93535b93d8407e14d1b8c6c78b476a72106155eb
5r1kanth <S560174@nwmissouri.edu>
2024-04-10 12:29:03 -0500

    Completed Screenshot 5

--- controllers/dragonEggs.js
+++ controllers/dragonEggs.js
@@ -8,17 +8,17 @@ exports.dragonEggs_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: dragonEggs detail: ' + req.params.id);
 };
 // Handle dragonEggs create on POST.
-exports.dragonEggs_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: dragonEggs create POST');
-};
+// exports.dragonEggs_create_post = function (req, res) {
+//     res.send('NOT IMPLEMENTED: dragonEggs create POST');
+// };
 // Handle dragonEggs delete from on DELETE.
 exports.dragonEggs_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: dragonEggs delete DELETE ' + req.params.id);
 };
 // Handle dragonEggs update form on PUT.
-exports.dragonEggs_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: dragonEggs update PUT' + req.params.id);
-};
+// exports.dragonEggs_update_put = function (req, res) {
+//     res.send('NOT IMPLEMENTED: dragonEggs update PUT' + req.params.id);
+// };
 
 // List of all dragonEggss
 exports.dragonEggs_list = async function (req, res) {
@@ -86,9 +86,9 @@ ${JSON.stringify(req.body)}`)
     try {
         let toUpdate = await dragonEggs.findById(req.params.id)
         // Do updates of properties
-        if (req.body.dragonEggs_type) toUpdate.dragonEggs_type = req.body.dragonEggs_type;
-        if (req.body.color) toUpdate.color = req.body.color;
+        // if (req.body.dragonEggs_type) toUpdate.dragonEggs_type = req.body.dragonEggs_type;
         if (req.body.size) toUpdate.size = req.body.size;
+        if (req.body.color) toUpdate.color = req.body.color;
         if (req.body.rarity) toUpdate.rarity = req.body.rarity;
         let result = await toUpdate.save();
         console.log("Sucess " + result)
@@ -97,6 +97,19 @@ ${JSON.stringify(req.body)}`)
         res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id} 
 failed`);
+    }
+};
+
+// Handle dragonEggs delete on DELETE.
+exports.dragonEggs_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await dragonEggs.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
     }
 };
 
