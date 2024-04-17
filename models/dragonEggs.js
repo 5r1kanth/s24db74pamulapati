506493e08a240bfdea9dc58b1b0cad71c4a79a8a
5r1kanth <S560174@nwmissouri.edu>
2024-04-17 14:36:16 -0500

    Completed

--- models/dragonEggs.js
+++ models/dragonEggs.js
@@ -1,8 +1,25 @@
 const mongoose = require("mongoose")
 const dragonEggSchema = mongoose.Schema({
-size: Number,
-color: String,
-rarity: String
-})
+
+    size: {
+        type : Number,
+        required: [true, "dragonEgg size is required"],
+        min: [1, "Size must be at least 1"],
+    },
+
+    color: {
+        type : String,
+        required: [true, "Dragon Egg color is required"],
+        minlength : [3, "Color must be at least 3"],
+    },
+
+    rarity: {
+        type : String,
+        required: [true, "Rarity is required"],
+        minlength : [3, "Rarity must be at least 3"],
+    }
+    })
+
+
 module.exports = mongoose.model("dragonEggs", 
 dragonEggSchema)
--- routes/dragonEggs.js
+++ routes/dragonEggs.js
@@ -12,27 +12,14 @@ const secured = (req, res, next) => {
  res.redirect("/login");
  }
 
-
-/* GET home page. */
-// router.get('/', function (req, res, next) {
-//   res.render('dragonEggs', { title: 'Search Results - Dragon Eggs' });
-// });
-
-
 const dragonEggs_controlers = require('../controllers/dragonEggs');
 
 router.get('/', dragonEggs_controlers.dragonEggs_view_all_Page);
 /* GET detail dragonEggs page */
 router.get('/detail', dragonEggs_controlers.dragonEggs_view_one_Page);
-/* GET create dragonEggs page */
-router.get('/create', dragonEggs_controlers.dragonEggs_create_Page);
-/* GET create update page */
-// router.get('/update', dragonEggs_controlers.dragonEggs_update_Page);
-/* GET delete dragonEggs page */
-router.get('/delete', dragonEggs_controlers.dragonEggs_delete_Page);
-
 /* GET update costume page */
-router.get('/update', secured, 
-dragonEggs_controlers.dragonEggs_update_Page);
+router.get('/create', secured, dragonEggs_controlers.dragonEggs_create_Page);
+router.get('/update', secured, dragonEggs_controlers.dragonEggs_update_Page);
+router.get('/delete', secured, dragonEggs_controlers.dragonEggs_delete_Page);
 
 module.exports = router;
